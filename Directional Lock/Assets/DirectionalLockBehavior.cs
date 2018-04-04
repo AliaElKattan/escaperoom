1bac5d48c46889795b6eedbc9746f85247a40336
alialrahma <alialrahma@Alis-MacBook-Pro.local>
2018-04-04 21:05:59 +0400

    add dir lock puzzle

---
+++ Directional Lock/Assets/DirectionalLockBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalLockBehavior : MonoBehaviour {
+
+	[SerializeField]Color defaultColor; 
+	[SerializeField]Color highlightColor; 
+	[SerializeField]float resetDelay = 0.2f;
+	AudioSource sound;
+
+	// initialize resetColor function (defaultColor) 
+	void Start () {
+		resetColor ();
+	}
+
+	// initialize audio source
+	void Awake() { 
+		sound = GetComponent<AudioSource> ();
+	}
+
+	/* when mouse is clicked, plays audio, changes color to 
+	 * highlightColor and resets to defaultColor */
+	void OnMouseDown() {
+		//Debug.Log("clicked");
+		sound.Play ();
+		GetComponent<MeshRenderer> ().material.color = highlightColor;
+		Invoke ("resetColor", resetDelay);
+	}
+
+	void resetColor() {
+		GetComponent<MeshRenderer> ().material.color = defaultColor;
+	}
+
+}
